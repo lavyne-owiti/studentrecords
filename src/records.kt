7253869404e3d4ddb9e9a3345c93480d19ac6745
lavyne <owitilavyne@gmail>
2022-02-17 08:56:52 +0300

    first assignment

---
+++ src/records.kt
@@ -0,0 +1,42 @@
+fun main(){
+    name()
+phoneNumber()
+age()
+    weight()
+    citizen()
+
+}
+
+fun name(){
+    val name = "lavyne Owiti"
+    println(name)
+}
+
+
+fun age(){
+    var age =21
+    age+=3
+    println(age)
+}
+fun weight() {
+    var weight = 70
+    weight -= 2
+    println(weight)
+}
+fun citizen(){
+    val kenyan = true
+    println(kenyan)
+
+}
+
+
+fun phoneNumber(){
+    val phoneNumber="0700245143"
+        println(phoneNumber)
+}
+
+
+
+
+
+
